de71c3a221446643083c5f2970b9832da9db25bd
Eileen Lu <s.eileen.lu@gmail.com>
2019-03-12 12:00:16 -0700

    reformat + added .gitignore file

--- constforkcount.py
+++ constforkcount.py
@@ -11,49 +11,50 @@ import requests
 import json
 import os
 import ast
+import settings
 from math import *
 from datetime import datetime
 
 # query block height through json-rpc infura api
 def get_blocknumber():
-	# unique project ID endpoint; remember the quotation marks
-	url = 'https://mainnet.infura.io/v3/cf03c330264a4bc5974a288edc5e4560'
-	
-	headers = {
-		'content-type': 'application/json',
-	}
-	
-	data = {
-		"jsonrpc":"2.0",
-		"method":"eth_blockNumber",
-		"params": [],
-		"id":1
-	}
+    # unique project ID endpoint; remember the quotation marks
+    url = settings.url
 
-	response = requests.post(url, headers=headers, data=json.dumps(data))
-	request_result = response.json()
-	# pull result key from response; returns hex value
-	block_hex = request_result["result"]
-	# convert hex value into integer
-	block_height = int(block_hex,0)
-	return block_height
+    headers = {
+        'content-type': 'application/json',
+    }
+    
+    data = {
+        "jsonrpc":"2.0",
+        "method":"eth_blockNumber",
+        "params": [],
+        "id":1
+    }
+
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    request_result = response.json()
+    # pull result key from response; returns hex value
+    block_hex = request_result["result"]
+    # convert hex value into integer
+    block_height = int(block_hex,0)
+    return block_height
 
 # Subtract latest block from Constantinople fork
 def block_calc(block_height):
-	# Block 7280000
-	if block_height <= 7280000:
-		const_block = 7280000 - int(block_height)
-		now=datetime.now()
-		print '%s remaining blocks until Constantinople fork! || %02d/%02d/%04d %02d:%02d:%02d' % (const_block,now.month,now.day,now.year,now.hour,now.minute,now.second)
-	else:
-		print 'Forked!'
-	return const_block
+    # Block 7280000
+    if block_height <= 7280000:
+        const_block = 7280000 - int(block_height)
+        now=datetime.now()
+        print '%s remaining blocks until Constantinople fork! || %02d/%02d/%04d %02d:%02d:%02d' % (const_block,now.month,now.day,now.year,now.hour,now.minute,now.second)
+    else:
+        print 'Forked!'
+    return const_block
 
 def main():
 
-	current_block = get_blocknumber()
-	block_calc(current_block)
+    current_block = get_blocknumber()
+    block_calc(current_block)
 
 if __name__ == "__main__":
-	main()
+    main()
 
